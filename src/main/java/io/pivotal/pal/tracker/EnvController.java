c6ae0e19ead6b650a572ffff63b1050bc56ea742
Shovan Biswas <shovan.biswas@accenture.com>
2018-10-30 08:23:34 -0700

    removing space to kick of build

--- src/main/java/io/pivotal/pal/tracker/EnvController.java
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -15,7 +15,6 @@ public class EnvController {
     private final String cfInstanceIndex;
     private final String cfInstanceAddress;
 
-
     public EnvController(
             @Value("${PORT:NOT SET}") String port,
             @Value("${MEMORY_LIMIT:NOT SET}") String memoryLimit,
